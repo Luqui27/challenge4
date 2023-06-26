82bbffb842b80a38bb2218e47e92f3ac824c9b73
Luqui27 <134293015+Luqui27@users.noreply.github.com>
2023-06-26 15:08:18 -0300

    Crea un programa que determine si un número ingresado por el usuario es par o impar.

---
+++ app.js
@@ -0,0 +1,8 @@
+//Crea un programa que determine si un número ingresado por el usuario es par o impar.
+
+let numero=parseInt(prompt("ingrese un numero"))
+if(numero%2==0){
+    alert("El numero ingresado es par")
+}else{
+    alert("El numero ingresado es Impar")
+}
